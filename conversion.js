e39a1856581e6a05eba83df82b349196ea98eb4f
Mateusz Sochalski <matisoch04@gmail.com>
2021-10-01 18:01:11 +0100

    Add files via upload

---
+++ conversion.js
@@ -0,0 +1,74 @@
+let outputArea = document.querySelector('.output');
+let inputType = document.querySelector('#inputType');
+
+
+function round(value) {
+    return Math.round((value + Number.EPSILON) * 10**4) / 10**4;
+}
+
+class Converter
+{
+    static kilogramsToAll(kg) {
+        return [["Grams", round(kg * 1000)],
+                ["Pounds", round(kg * 0.00220462)],
+                ["Ounces", round(kg * 35.274)]
+            ]
+    }
+
+    static gramsToAll(grams) {
+        return [
+                ["Kilograms", round(grams / 1000)],
+                ["Pounds", round(grams * 0.00220462)],
+                ["Ounces", round(grams * 0.0352739200000000003)]
+            ]
+    }
+
+    static ouncesToAll(ounces) {
+        return [
+            ["Kilograms", round(ounces / 35.274)],
+            ["Grams", round(ounces / 16)],
+            ["Pounds", round(ounces * 28.3495)]
+        ]
+    }
+
+    static poundsToAll(pounds) {
+        return [
+            ["Kilograms", round(pounds * 16)],
+            ["Grams", round(pounds * 0.453592)],
+            ["Ounces", round(pounds * 453.5920000001679)]
+        ]
+    }
+}
+
+function updateValues() {
+    let input = document.querySelector('#weight').value;
+    switch(inputType.value){
+        case "kilograms": updateOutput(Converter.kilogramsToAll(input));
+            break;
+        case "grams": updateOutput(Converter.gramsToAll(input));
+            break;
+        case "pounds": updateOutput(Converter.poundsToAll(input));
+            break;
+        case "ounces": updateOutput(Converter.ouncesToAll(input));
+    }
+}
+
+function updateOutput(values) {
+    outputArea.style.visibility = 'visible';
+
+    document.querySelector('#firstOutputName').innerHTML = values[0][0];
+    document.querySelector('#firstOutputValue').innerHTML = values[0][1];
+
+    document.querySelector('#secondOutputName').innerHTML = values[1][0];
+    document.querySelector('#secondOutputValue').innerHTML = values[1][1];
+
+    document.querySelector('#thirdOutputName').innerHTML = values[2][0];
+    document.querySelector('#thirdOutputValue').innerHTML = values[2][1];
+}
+
+inputType.addEventListener('change', (event) => {
+    if(document.querySelector('#weight').value != 0)
+    updateValues()
+});
+
+document.querySelector('#weight').addEventListener('input', (event) => updateValues());
